69f32f183ef71dd8ddc08807b299379f9e95faac
R.K.H.K.Ramanayaka <146801854+HiruniRamanayaka@users.noreply.github.com>
2025-04-09 00:04:03 +0530

    Add the full project via file uploading

---
+++ script.js
@@ -0,0 +1,59 @@
+const cells = document.querySelectorAll(".cell");
+const statusText = document.getElementById("status");
+const restartBtn = document.getElementById("restart-btn");
+
+let board = ["", "", "", "", "", "", "", "", ""];
+let currentPlayer = "X";
+let gameActive = true;
+
+const winningConditions = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], 
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], 
+    [0, 4, 8], [2, 4, 6]
+];
+
+cells.forEach(cell => cell.addEventListener("click", cellClicked));
+restartBtn.addEventListener("click", restartGame);
+
+function cellClicked() {
+    const index = this.getAttribute("data-index");
+
+    if (board[index] !== "" || !gameActive) return;
+
+    board[index] = currentPlayer;
+    this.innerText = currentPlayer;
+    checkWinner();
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+}
+
+function checkWinner() {
+    let roundWon = false;
+    for (let condition of winningConditions) {
+        const [a, b, c] = condition;
+        if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+            roundWon = true;
+            break;
+        }
+    }
+
+    if (roundWon) {
+        statusText.innerText = `Player ${currentPlayer === "X" ? "O" : "X"} Wins!`;
+
+        gameActive = false;
+        return;
+    }
+
+    if (!board.includes("")) {
+        statusText.innerText = "It's a Draw!";
+        gameActive = false;
+        return;
+    }
+}
+
+function restartGame() {
+    board = ["", "", "", "", "", "", "", "", ""];
+    gameActive = true;
+    currentPlayer = "X";
+    statusText.innerText = "";
+    cells.forEach(cell => cell.innerText = "");
+}
